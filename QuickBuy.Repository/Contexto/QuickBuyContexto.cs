c2fad3c0777150f1efb7ca7295ddbd0d0356e4d8
lucasilva95 <lucascsilva95@gmail.com>
2020-03-22 12:52:23 -0300

    Realizada a instalação do Entity Framework Core e criado o diretório QuickBuyContexto para referenciar as entidades para a base de dados

---
+++ QuickBuy.Repository/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entidades;
+using QuickBuy.Domain.ObjetoDeValor;
+
+namespace QuickBuy.Repository.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+    }
+}
